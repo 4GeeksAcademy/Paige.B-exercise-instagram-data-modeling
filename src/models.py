1fce97179102adbc125308a21ff8d021e3773827
Techprenuer1 <125614142+Techprenuer1@users.noreply.github.com>
2023-06-14 22:02:42 +0000

    finished project

--- src/models.py
+++ src/models.py
@@ -27,7 +27,33 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+class Users(Base):
+    __tablename__ ="users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(256), nullable=False)
+    firstname = Column(String(256), nullable=False)
+    lastname = Column(String(256), nullable=False)
+    email = Column(String(256), nullable=False)
 
+class Comment(Base):
+    __tablename__="comment"
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(256))
+    author_id = Column(Integer, ForeignKey('author.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Post(Base):
+    __tablename__="post"
+    id = Column(Integer, primary_key=True)
+    post_text = Column(String(256))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Media(Base):
+    __tablename__="media"
+    id = Column(Integer, primary_key=True)
+    url = Column(String(256))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
